5d06bdaf3dd6134f4ee39b121815929eee70e21c
Piai <prigaissow@gmail.com>
2020-03-25 15:03:54 -0300

    Primeiro projeto feito, e agora servindo de testes para git

---
+++ 1-aula/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace _1_aula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mamaki");
+            Console.ReadLine();
+        }
+    }
+}
